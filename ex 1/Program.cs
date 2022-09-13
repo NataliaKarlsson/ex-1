b4dc68b3ca48611875c555de45fdcd3bc30c5615
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-13 14:10:34 +0200

    Add project files.

---
+++ ex 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 to 10: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
+        }
+    }
+}
